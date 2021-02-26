3c5748e0266f961a0d5c1df28a4695e35b20116e
Hanson Nguyen <hansonn00@gmail.com>
2021-02-25 21:30:38 -0500

    Update components

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -22,10 +22,6 @@ class App extends Component {
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/snacks">Snack List</Nav.Link>
             </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-info">Search</Button>
-              </Form>
           </Navbar>
         <br />
             <Route path ="/snacks" exact component = {SnackList} />
--- Code/frontend/src/components/snack-list.component.js
+++ Code/frontend/src/components/snack-list.component.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import styles from "./styles.module.css";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { sort } from './sort.js';
 
 //<td>{props.snacks._id}</td>
 
