6255b01b3cbb7c3923afd37fb9db9c661279d731
Hanson Nguyen <hansonn00@gmail.com>
2021-03-15 15:37:32 -0400

    update search function

--- Code/frontend/src/components/search.js
+++ Code/frontend/src/components/search.js
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import React, { useState } from "react"; 
-import axios from "axios";
+  import axios from "axios";
 
 export function storeInput() {
 
     let input = document.getElementById("a").value;
+    let result = [];
 
     console.log(input);   
 
     axios.post("http://localhost:4000/id", {
         searchWord: input,
       }).then((response) => {
-        console.log(response);
+        result = response.data;
+        console.log(result);
+
+        return result;
       });
 }
 
