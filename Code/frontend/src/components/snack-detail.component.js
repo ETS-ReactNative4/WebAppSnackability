cd12acfa182251f7b59c2489f23caf1bbcc9d6ba
JamesO97 <60301951+JamesO97@users.noreply.github.com>
2021-03-29 21:21:48 -0400

    Update snack-detail.component.js

--- Code/frontend/src/components/snack-detail.component.js
+++ Code/frontend/src/components/snack-detail.component.js
@@ -5,43 +5,6 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import axios from 'axios';
 
 
-const Snacks = ({ snacks }) => (
-    <tr>
-        <td>{snacks._id}</td>
-        <td>{snacks.brand_name}</td>
-        <td>{snacks.product}</td>
-        <td>{snacks.short_name}</td>
-        <td>{snacks.serving_size}</td>
-        <td>{snacks.calories}</td>
-        <td>{snacks.calories_fat}</td>
-        <td>{snacks.saturated_fat}</td>
-        <td>{snacks.trans_fat}</td>
-        <td>{snacks.sodium}</td>
-        <td>{snacks.sugar}</td>
-        <td>{snacks.first_ingredient}</td>
-        <td>{snacks.processed}</td>
-
-        <td className="operation">
-                <a href='/details' data-id={snacks._id}><button
-                 // className= {styles.buttonCalculate}
-                  //onClick={() => calculateData(snacks._id)}
-                >
-                  Calculate
-                </button></a>
-        </td>
-    </tr>
-);
-
-function testinValue(){    
-    window.localStorage.setItem('MySavedValue', document.getElementById("a").value); // save data
-    window.location.reload();
-}
-
-const calculateData = async (SnackID) => {
-
-    alert("Redirecting to the calculate Page");
-    alert("You Clicked on Row with ID == " + SnackID);
-};
 
 export default class SnackDetail extends Component {
     
@@ -51,28 +14,37 @@ export default class SnackDetail extends Component {
             this.id = "";          
     }
     
-    setSnackState(snacks = null) {
-        console.log("WTF");
-        console.log(snacks);
+    setSnackState(snacks) {
 
         this.setState({ snacks });    
     }
     
     componentDidMount() {
-        let input = window.localStorage.getItem('MySavedValue');
-        let inputID = window.localStorage.getItem('MySavedidValue');
-        const callback = (snacks) => this.setSnackState(snacks);
-        storeInput(inputID, callback);
+        //let input = window.localStorage.getItem('MySavedValue');
+        let inputID = window.localStorage.getItem('SavedSnackToCalc');
+        this.state.snacks = JSON.parse(inputID);
+        //this.setSnackState(JSON.parse(inputID));
+        //const callback = (snacks) => this.setSnackState(snacks);
+        //storeInput(inputID, callback);
     }
 
     SnackList() {
-        const snacksList = this.state.snacks;
+        let inputID = window.localStorage.getItem('SavedSnackToCalc');
+        console.log(inputID);
+        this.state.snacks = JSON.parse(inputID);
         return (
-          snacksList &&
-          snacksList.map((currentSnack, i) => (
-            <Snacks snacks={currentSnack} key={i} />
-          ))
-        );
+            <tbody>
+            <tr><td>First Ingredient</td><td>{this.state.snacks.first_ingredient}</td>2<td></td></tr>
+            <tr><td>Calories</td><td>{this.state.snacks.calories}</td>2<td></td></tr>
+            <tr><td>Saturated Fats</td><td>{this.state.snacks.saturated_fat}</td>0.5<td></td></tr>
+            <tr><td>Trans Fat</td><td></td><td></td></tr>
+            <tr><td>Sodium</td><td></td><td></td></tr>
+            <tr><td>Sugars:</td><td></td><td></td></tr>
+            <tr><td>Highly Processed</td><td></td><td></td></tr> 
+            <tr><td>Total</td><td></td></tr> 
+        <tr></tr>
+        </tbody>
+            );
     }
 
     render() {
@@ -81,32 +53,17 @@ export default class SnackDetail extends Component {
         return (
             <div className = "searcho">
 
-
-
-            
                 <table id = "myTable" className={styles.table} /*style={{ marginTop: 20 }}*/ >
                     <thead>
                         
                         <tr>
-                            <th>Object ID</th>
-                            <th>Brand Name</th>
-                            <th>Product</th>
-                            <th>Short Name</th>
-                            <th>Serving Size</th>
-                            <th>Calories</th>
-                            <th>Calories Fat</th>
-                            <th>Saturated Fat</th>
-                            <th>Trans Fat</th>
-                            <th>Sodium</th>
-                            <th>Sugar</th>
-                            <th>First Ingredient</th>
-                            <th>Processed</th>
-                            <th></th>
+                            <th>Criteria</th>
+                            <th>Score</th>
+                            <th>Max Score</th>
+                            
                         </tr>
                     </thead>
-                    <tbody>
-                        {this.id}
-                    </tbody>
+                    { this.SnackList() }
                 </table>
             </div>
         )
