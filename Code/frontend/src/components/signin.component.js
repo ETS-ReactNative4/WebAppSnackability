02760a3aa8f5da6223a57876747efce0ef0b36b0
4UR3L10 <aureliomartinezdros@outlook.com>
2021-05-31 14:59:41 -0400

    New Improvements
    +NBCSC+

--- Code/frontend/src/components/signin.component.js
+++ Code/frontend/src/components/signin.component.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 
-import {/* Card, Col, Container, */Jumbotron/*, Row */} from 'react-bootstrap';
+import {/* Card, Col, Container, *//*Jumbotron,*/ Button/*, Row */} from 'react-bootstrap';
 import "../styles/home.css"
 
 
-export default class HomeComponent extends Component {
+/*import ReactDOM from 'react-dom'*/
+import Scanner from './Scanner'
+import Result from './Result'
+
+
+export default class HomeComponent extends Component {   
+    state = {
+        scanning: false,
+        results: [],
+    }
+
+    _scan = () => {
+        this.setState({ scanning: !this.state.scanning })
+    }
+    
+    _onDetected = result => {
+        this.setState({ results: this.state.results.concat([result]) })
+    }
 
     render() {
-        return (
-
+        return (      
             <div>
+                <div>
+                    <Button variant="primary" onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Or Scan the Barcode!'} 📷</Button>
+
+                    <ul className="results">
+                        {this.state.results.map((result, i) => (
+                            <Result key={result.codeResult.code + i} result={result} />                            
+                        ))}
+                    </ul>
+
+                    {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null}
+                </div>
+                
+                {/*
                 <Jumbotron fluid className="position-absolute w-100 text-center mt-4">
                     <h6>Contact the Product Owner</h6>
                     <p>For questions or to provide feedback in regards to the Snackability app, please email
@@ -17,6 +46,7 @@ export default class HomeComponent extends Component {
                     <p>crpalaci@fiu.edu</p>
                     <p>snackabilityapp@gmail.com</p>
                 </Jumbotron>
+                */}
 
             </div>
         );
