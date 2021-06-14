f7ae18cc6c3b97daa7edd4724ba787484d86a4c8
onel-sanchez <60905558+onel-sanchez@users.noreply.github.com>
2021-06-14 00:20:56 -0400

    Route /snack_score and fetchSnackScore from service frontend.
    Created a route for snack_score and the service from the front end to get that function.

--- Code/backend/routes/usda.routes.js
+++ Code/backend/routes/usda.routes.js
@@ -1,53 +1,64 @@
 const express = require('express');
-
+const firtIng = require('../helpers/scoringAlgorthim');
 const {
-    getUSDASnacks,
-    getUSDASnackById,
-    searchUSDASnack
+	getUSDASnacks,
+	getUSDASnackById,
+	searchUSDASnack,
 } = require('../controllers/usda.controller');
+const firstIng = require('../helpers/scoringAlgorthim');
 
 const router = express.Router();
 
-router.get('/snacks', async (req,res, next) => {
-    try {
-        const snacks = await getUSDASnacks();
-        res.json(snacks.data);
-    } catch (err) {
-        console.log(err)
-        res.send(err);
-    }
+router.get('/snacks', async (req, res, next) => {
+	try {
+		const snacks = await getUSDASnacks();
+		res.json(snacks.data);
+	} catch (err) {
+		console.log(err);
+		res.send(err);
+	}
 });
 
 router.get('/snacks/:snack_id', async (req, res, next) => {
+	const snack_id = req.params.snack_id;
 
-    const snack_id = req.params.snack_id;
+	if (!snack_id) {
+		res.status(400);
+		res.send('Missing snack_id');
+	}
 
-    if(!snack_id) {
-        res.status(400);
-        res.send("Missing snack_id");
-    }
-
-    try {
-       const snack = await getUSDASnackById(snack_id)
-       res.json(snack.data.foods);
-    } catch (err) {
-        console.log(err)
-        throw err;
-    }
-
+	try {
+		const snack = await getUSDASnackById(snack_id);
+		res.json(snack.data.foods);
+	} catch (err) {
+		console.log(err);
+		throw err;
+	}
 });
 
-router.get('/search', async (req,res, next) => {
+router.get('/search', async (req, res, next) => {
+	const q = req.query.q || '';
 
-    const q = req.query.q || "";
-    
-    try {
-        const snacks = await searchUSDASnack(q);
-        res.json(snacks.data.foods);    
-    } catch (err) {
-        console.log(err)
-        res.send(err);
-    }
+	try {
+		const snacks = await searchUSDASnack(q);
+		res.json(snacks.data.foods);
+	} catch (err) {
+		console.log(err);
+		res.send(err);
+	}
+});
+
+router.get('/snack_score', async (req, res, next) => {
+	const q = req.query;
+
+	try {
+		const snack = await getUSDASnackById(q.snack_id);
+		var score = firstIng(snack.ingredients);
+		res.send({ firstIngScore: score });
+	} catch (err) {
+		console.log(err);
+		throw err;
+	}
 });
 
 module.exports = router;
--- Code/frontend/src/services/snack.service.js
+++ Code/frontend/src/services/snack.service.js
@@ -1,34 +1,43 @@
 import axios from 'axios';
 
 export function fetchSnacksList() {
-    return axios.get('http://localhost:4000/snacks');
+	return axios.get('http://localhost:4000/snacks');
 }
 
 export function fetchSnacksByName(keyword) {
-    return axios.get('http://localhost:4000/snacks', {
-        params: {
-            q: keyword
-        }
-    });
+	return axios.get('http://localhost:4000/snacks', {
+		params: {
+			q: keyword,
+		},
+	});
 }
 
 export function fetchSnackByID(snack_id) {
-    return axios.get(`http://localhost:4000/snacks/${snack_id}`);
+	return axios.get(`http://localhost:4000/snacks/${snack_id}`);
 }
 
 export function fetchSnacksListUSDA() {
-    return axios.get('http://localhost:4000/usda/snacks');
+	return axios.get('http://localhost:4000/usda/snacks');
 }
 
 export function fetchSnacksByNameUSDA(keyword) {
-    return axios.get('http://localhost:4000/usda/search', {
-        params: {
-            q: keyword
-        }
-    });
+	return axios.get('http://localhost:4000/usda/search', {
+		params: {
+			q: keyword,
+		},
+	});
 }
 
 export function fetchSnackByIDUSDA(snack_id) {
-    return axios.get(`http://localhost:4000/usda/snacks/${snack_id}`);
+	return axios.get(`http://localhost:4000/usda/snacks/${snack_id}`);
 }
 
+export function fetchSnackScore(snack_id, serving, unit) {
+	return axios.get('http://localhost:4000/usda/snack_score', {
+		params: {
+			snack_id: snack_id,
+			serving: serving,
+			unit: unit,
+		},
+	});
+}
