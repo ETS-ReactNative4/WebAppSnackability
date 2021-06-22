e7a9195eacd86a7092f2337b6894ce4802e056a3
onel-sanchez <60905558+onel-sanchez@users.noreply.github.com>
2021-06-22 02:56:45 -0400

    Scanner style

--- Code/frontend/src/components/Scanner.jsx
+++ Code/frontend/src/components/Scanner.jsx
@@ -1,50 +1,113 @@
-import React, { Component } from 'react'
-import Quagga from 'quagga'
+import React, { Component } from 'react';
+import Quagga from 'quagga';
+import styles from '../styles/scanner.css';
 
 class Scanner extends Component {
-  componentDidMount() {
-    Quagga.init(
-      {
-        inputStream: {
-          type: 'LiveStream',
-          constraints: {
-            width: 640,
-            height: 480,
-            facingMode: 'environment', // or user
-          },
-        },
-        locator: {
-          patchSize: 'medium',
-          halfSample: true,
-        },
-        numOfWorkers: 4,
-        decoder: {
-          readers: ['code_128_reader','ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader','i2of5_reader','2of5_reader','code_93_reader'],
-          
-        },
-        locate: true,
-      },
-      function(err) {
-        if (err) {
-          return console.log(err)
-        }
-        Quagga.start()
-      },
-    )
-    Quagga.onDetected(this._onDetected)
-  }
+	componentDidMount() {
+		Quagga.init(
+			{
+				inputStream: {
+					type: 'LiveStream',
+					constraints: {
+						width: 640,
+						height: 480,
+						facingMode: 'environment', // or user
+					},
+					area: {
+						// defines rectangle of the detection/localization area
+						top: '0%', // top offset
+						right: '0%', // right offset
+						left: '0%', // left offset
+						bottom: '0%', // bottom offset
+					},
+				},
+				locator: {
+					patchSize: 'medium',
+					halfSample: true,
+				},
+				numOfWorkers: 4,
+				decoder: {
+					readers: [
+						'code_128_reader',
+						'ean_reader',
+						'ean_8_reader',
+						'code_39_reader',
+						'code_39_vin_reader',
+						'codabar_reader',
+						'upc_reader',
+						'upc_e_reader',
+						'i2of5_reader',
+						'2of5_reader',
+						'code_93_reader',
+					],
+				},
+				locate: true,
+			},
+			function (err) {
+				if (err) {
+					return console.log(err);
+				}
+				Quagga.start();
+			}
+		);
+		Quagga.onDetected(this._onDetected);
+		Quagga.onProcessed(this._onProcessed);
+	}
 
-  componentWillUnmount() {
-    Quagga.offDetected(this._onDetected)
-  }
+	componentWillUnmount() {
+		Quagga.offDetected(this._onDetected);
+	}
 
-  _onDetected = result => {
-    this.props.onDetected(result)
-  }
+	_onDetected = (result) => {
+		Quagga.stop();
+		this.props.onDetected(result);
+	};
 
-  render() {
-    return <div id="interactive" className="viewport" />
-  }
+	_onProcessed(result) {
+		let drawingCtx = Quagga.canvas.ctx.overlay,
+			drawingCanvas = Quagga.canvas.dom.overlay;
+
+		if (result) {
+			if (result.boxes) {
+				drawingCtx.clearRect(
+					0,
+					0,
+					parseInt(drawingCanvas.getAttribute('width'), 10),
+					parseInt(drawingCanvas.getAttribute('height'), 10)
+				);
+				result.boxes
+					.filter(function (box) {
+						return box !== result.box;
+					})
+					.forEach(function (box) {
+						Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+							color: 'green',
+							lineWidth: 2,
+						});
+					});
+			}
+
+			if (result.box) {
+				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+					color: '#00F',
+					lineWidth: 2,
+				});
+			}
+
+			if (result.box) {
+				Quagga.ImageDebug.drawPath(
+					result.line,
+					{ x: 'x', y: 'y' },
+					drawingCtx,
+					{ color: 'red', lineWidth: 3 }
+				);
+			}
+		}
+	}
+
+	render() {
+		return <div id='interactive' className='viewport' />;
+	}
 }
 
-export default Scanner
+export default Scanner;
--- Code/frontend/src/components/snack-usda-list.component.js
+++ Code/frontend/src/components/snack-usda-list.component.js
@@ -1,138 +1,177 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import { fetchSnacksListUSDA,fetchSnacksByNameUSDA } from '../services/snack.service.js'; 
+import {
+	fetchSnacksListUSDA,
+	fetchSnacksByNameUSDA,
+} from '../services/snack.service.js';
 import { debounce } from '../utils/debounce';
 
 import styles from '../styles/styles.module.css';
 
-import { Button, Col, Container, Form, Row, Table} from 'react-bootstrap';
-import Scanner from './Scanner'
+import {
+	Button,
+	Col,
+	Container,
+	Form,
+	Row,
+	Table,
+	Modal,
+} from 'react-bootstrap';
+import Scanner from './Scanner';
 //import Result from './Result'
 
-const Snacks = ({snacks}) => (
-    <tr>
-        <td>
-            <Link to={`/usda/${snacks.fdcId}`}>{snacks.description}</Link><br/>
-            <span className="text-muted">{snacks.brandOwner}</span>
-        </td>                                 
-    </tr>   
+const Snacks = ({ snacks }) => (
+	<tr>
+		<td>
+			<Link to={`/usda/${snacks.fdcId}`}>{snacks.description}</Link>
+			<br />
+			<span className='text-muted'>{snacks.brandOwner}</span>
+		</td>
+	</tr>
 );
 
 export default class SnackList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			snacks: [],
+			isLoading: false,
+			scanning: false,
+			results: [],
+			lastBarcode: null,
+		};
+		this._toggle = this._toggle.bind(this);
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            snacks: [], 
-            isLoading: false,     
-            scanning: false,
-            results: [],
-            lastBarcode: null       
-        };
-    }
+	_scan = () => {
+		this.setState((prevState) => ({
+			modal: !prevState.modal,
+		}));
+		this.setState({ scanning: !this.state.scanning });
+	};
 
-    _scan = () => {
-        this.setState({ scanning: !this.state.scanning })
-    }
-    
-    _onDetected = result => {
-        console.log('result.codeResult.code: ' + result.codeResult.code);
-        console.log('this.state.lastBarcode: ' + this.state.lastBarcode);
-        
-        if (this.state.lastBarcode || result.codeResult.code !== this.state.lastBarcode) {
-            this.searchForItem(result.codeResult.code);
-            console.log('THE RESULT IS: ' + result.codeResult.code);   
-        }        
+	_toggle() {
+		this.setState((prevState) => ({
+			modal: !prevState.modal,
+		}));
+	}
 
-        this.setState({ results: this.state.results.concat([result]), lastBarcode: result.codeResult.code});
-    }
+	_onDetected = (result) => {
+		console.log('result.codeResult.code: ' + result.codeResult.code);
+		console.log('this.state.lastBarcode: ' + this.state.lastBarcode);
 
-    componentDidMount() {
-        this.searchForItem("");
-    }
+		if (
+			this.state.lastBarcode ||
+			result.codeResult.code !== this.state.lastBarcode
+		) {
+			this.searchForItem(result.codeResult.code);
+			console.log('THE RESULT IS: ' + result.codeResult.code);
+		}
 
-    fetchSnacksListUSDA() {
-        
-        this.setState({ isLoading: true });
+		this.setState({
+			results: this.state.results.concat([result]),
+			lastBarcode: result.codeResult.code,
+		});
+		this.setState((prevState) => ({
+			modal: !prevState.modal,
+		}));
+	};
 
-        fetchSnacksListUSDA()
-            .then(response => response.data)            
-            .then((snacks) => {                               
-                this.setState({ snacks: snacks, isLoading: false });
-            })
-            .catch(error => {
-                console.error(error);
-                this.setState({ isLoading: false });
-            })
-    }
+	componentDidMount() {
+		this.searchForItem('');
+	}
 
-    searchForItem(keyword) {
-        debounce(() => {
-            this.setState({ isLoading: true });
-            fetchSnacksByNameUSDA(keyword)
-                .then(response => response.data)
-                .then((snacks) => {
-                    this.setState({ snacks: snacks, isLoading: false });
-                })
-                .catch(error => {
-                    console.error(error);
-                    this.setState({ isLoading: false });
-                });
-        }, 1000);
-    }
+	fetchSnacksListUSDA() {
+		this.setState({ isLoading: true });
 
-    SnackList() {
-        const snacksList = this.state.snacks; 
-        
-        return (            
-            snacksList &&
-            snacksList.map((currentSnack, i) => (
-                <Snacks snacks={ currentSnack } key={i}/>
-            ))
-        );
-    }
+		fetchSnacksListUSDA()
+			.then((response) => response.data)
+			.then((snacks) => {
+				this.setState({ snacks: snacks, isLoading: false });
+			})
+			.catch((error) => {
+				console.error(error);
+				this.setState({ isLoading: false });
+			});
+	}
 
-    render() {
+	searchForItem(keyword) {
+		debounce(() => {
+			this.setState({ isLoading: true });
+			fetchSnacksByNameUSDA(keyword)
+				.then((response) => response.data)
+				.then((snacks) => {
+					this.setState({ snacks: snacks, isLoading: false });
+				})
+				.catch((error) => {
+					console.error(error);
+					this.setState({ isLoading: false });
+				});
+		}, 1000);
+	}
 
-        return (
-            
-            <Container fluid  className="mt-5">    
-                <Row>
-                    <Col>        
-                        <Form.Group controlId="formControlPortion">
-                            <Form.Control id="a" type="search" placeholder="🔎 Search for a snack's brand name..."             
-                                onKeyUp={ (event) => this.searchForItem(event.target.value) }          
-                            />
-                       </Form.Group>
-                    </Col>
-                </Row>                  
-            
-                <Button variant="primary" onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Or Scan the Barcode!'} 📷</Button>                                                             
-                
-                {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null}                     
+	SnackList() {
+		const snacksList = this.state.snacks;
 
-                <Row className="mt-1">
-                    <Col>
-                        <Table striped hover>
-                            <thead>
-                            <tr>
-                                <th>Product</th>                                                   
-                            </tr>
-                            </thead>
-                            <tbody>
-                                <tr className={this.state.isLoading ? 'd-table-row' : 'd-none'}>
-                                    <td colSpan="12" className="text-center">Loading...</td>
-                                </tr>
-                                { !this.state.isLoading && this.SnackList() }                        
-                            </tbody>
-                            {console.log(this.state.snacks)}                                                        
-                        </Table>
-                    </Col>
-                </Row>          
-            </Container>
-        );
+		return (
+			snacksList &&
+			snacksList.map((currentSnack, i) => (
+				<Snacks snacks={currentSnack} key={i} />
+			))
+		);
+	}
 
-    }
+	render() {
+		return (
+			<Container fluid className='mt-5'>
+				<Row>
+					<Col>
+						<Form.Group controlId='formControlPortion'>
+							<Form.Control
+								id='a'
+								type='search'
+								placeholder="🔎 Search for a snack's brand name..."
+								onKeyUp={(event) => this.searchForItem(event.target.value)}
+							/>
+						</Form.Group>
+					</Col>
+				</Row>
 
+				<Button variant='primary' onClick={this._scan}>
+					Scan Barcode! 📷
+				</Button>
+
+				{/* {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null} */}
+
+				<Modal show={this.state.modal} onHide={this._toggle}>
+					<Modal.Header closeButton='true' />
+					<Modal.Body>
+						<Scanner onDetected={this._onDetected} />
+					</Modal.Body>
+				</Modal>
+
+				<Row className='mt-1'>
+					<Col>
+						<Table striped hover>
+							<thead>
+								<tr>
+									<th>Product</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr className={this.state.isLoading ? 'd-table-row' : 'd-none'}>
+									<td colSpan='12' className='text-center'>
+										Loading...
+									</td>
+								</tr>
+								{!this.state.isLoading && this.SnackList()}
+							</tbody>
+							{console.log(this.state.snacks)}
+						</Table>
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
 }
