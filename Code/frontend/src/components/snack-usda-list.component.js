c15fc3e9ae5807b467ac692e9e53324264fa2d2e
Thiago Novaes <tbnovaes.dev@gmail.com>
2021-06-27 21:45:32 -0400

    refactoring: refactored usda details code

--- Code/frontend/src/components/snack-usda-list.component.js
+++ Code/frontend/src/components/snack-usda-list.component.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import { fetchSnacksListUSDA,fetchSnacksByNameUSDA } from '../services/snack.service.js'; 
+import { fetchSnacksListUSDA,fetchSnacksByNameUSDA } from '../services/snack.service.js';
 import { debounce } from '../utils/debounce';
-
-import styles from '../styles/styles.module.css';
 
 import { Button, Col, Container, Form, Row, Table} from 'react-bootstrap';
 import Scanner from './Scanner'
@@ -15,8 +13,8 @@ const Snacks = ({snacks}) => (
         <td>
             <Link to={`/usda/${snacks.fdcId}`}>{snacks.description}</Link><br/>
             <span className="text-muted">{snacks.brandOwner}</span>
-        </td>                                 
-    </tr>   
+        </td>
+    </tr>
 );
 
 export default class SnackList extends Component {
@@ -24,26 +22,26 @@ export default class SnackList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            snacks: [], 
-            isLoading: false,     
+            snacks: [],
+            isLoading: false,
             scanning: false,
             results: [],
-            lastBarcode: null       
+            lastBarcode: null
         };
     }
 
     _scan = () => {
         this.setState({ scanning: !this.state.scanning })
     }
-    
+
     _onDetected = result => {
         console.log('result.codeResult.code: ' + result.codeResult.code);
         console.log('this.state.lastBarcode: ' + this.state.lastBarcode);
-        
+
         if (this.state.lastBarcode || result.codeResult.code !== this.state.lastBarcode) {
             this.searchForItem(result.codeResult.code);
-            console.log('THE RESULT IS: ' + result.codeResult.code);   
-        }        
+            console.log('THE RESULT IS: ' + result.codeResult.code);
+        }
 
         this.setState({ results: this.state.results.concat([result]), lastBarcode: result.codeResult.code});
     }
@@ -53,12 +51,12 @@ export default class SnackList extends Component {
     }
 
     fetchSnacksListUSDA() {
-        
+
         this.setState({ isLoading: true });
 
         fetchSnacksListUSDA()
-            .then(response => response.data)            
-            .then((snacks) => {                               
+            .then(response => response.data)
+            .then((snacks) => {
                 this.setState({ snacks: snacks, isLoading: false });
             })
             .catch(error => {
@@ -83,9 +81,9 @@ export default class SnackList extends Component {
     }
 
     SnackList() {
-        const snacksList = this.state.snacks; 
-        
-        return (            
+        const snacksList = this.state.snacks;
+
+        return (
             snacksList &&
             snacksList.map((currentSnack, i) => (
                 <Snacks snacks={ currentSnack } key={i}/>
@@ -96,40 +94,40 @@ export default class SnackList extends Component {
     render() {
 
         return (
-            
-            <Container fluid  className="mt-5">    
+
+            <Container fluid  className="mt-5">
                 <Row>
-                    <Col>        
+                    <Col>
                         <Form.Group controlId="formControlPortion">
-                            <Form.Control type="search" placeholder="🔎 Search for a snack's brand name..."             
-                                onKeyUp={ (event) => this.searchForItem(event.target.value) }          
+                            <Form.Control type="search" placeholder="🔎 Search for a snack's brand name..."
+                                onKeyUp={ (event) => this.searchForItem(event.target.value) }
                             />
                        </Form.Group>
                     </Col>
-                </Row>                  
-            
-                <Button variant="primary" onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Or Scan the Barcode!'} 📷</Button>                                                             
-                
-                {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null}                     
+                </Row>
+
+                <Button variant="primary" onClick={this._scan}>{this.state.scanning ? 'Stop' : 'Or Scan the Barcode!'} 📷</Button>
+
+                {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null}
 
                 <Row className="mt-1">
                     <Col>
                         <Table striped hover>
                             <thead>
                             <tr>
-                                <th>Product</th>                                                   
+                                <th>Product</th>
                             </tr>
                             </thead>
                             <tbody>
                                 <tr className={this.state.isLoading ? 'd-table-row' : 'd-none'}>
                                     <td colSpan="12" className="text-center">Loading...</td>
                                 </tr>
-                                { !this.state.isLoading && this.SnackList() }                        
+                                { !this.state.isLoading && this.SnackList() }
                             </tbody>
-                            {console.log(this.state.snacks)}                                                        
+                            {console.log(this.state.snacks)}
                         </Table>
                     </Col>
-                </Row>          
+                </Row>
             </Container>
         );
 
