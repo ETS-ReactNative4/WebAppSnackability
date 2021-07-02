65a08dc6592467ac6f8917082addb25df5815842
lsoca007 <77649258+lsoca007@users.noreply.github.com>
2021-07-02 19:10:37 -0400

    Update graph component

--- Code/frontend/src/components/snack-graph.component.js
+++ Code/frontend/src/components/snack-graph.component.js
@@ -1,42 +1,117 @@
+import axios from 'axios';
+import Chart from 'chart.js/auto';
+import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 
-import SnackDetailsStyles from '../styles/snack-details.module.css';
 
 const SnackGraph = () => {
-	return (
-		<div className={SnackDetailsStyles.div}>
-			<Bar
-				data={{
-					labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-					datasets: [
+
+	const [chartData, setChartData] = useState({});
+	const [snackPoints, setsnackPoints] = useState({});
+	const [employeeAge, setEmployeeAge] = useState({});
+
+
+	const chart = () => {
+
+
+		let snackPoints = [];
+		let empAge = [];
+
+		var day1 = moment().format('MMMM-DD');
+		var day2 = moment().subtract(1, 'days').format('MMMM-DD');
+		var day3 = moment().subtract(2, 'days').format('MMMM-DD');
+		var day4 = moment().subtract(3, 'days').format('MMMM-DD');
+		var day5 = moment().subtract(4, 'days').format('MMMM-DD');
+
+		axios.get("http://www.json-generator.com/api/json/get/bRuxWOIrjC?indent=2")
+
+			.then(res => {
+				console.log(res)
+
+				for (const dataObj of res.data.data) {
+					snackPoints.push(parseInt(dataObj.snack_points))
+					empAge.push(parseInt(dataObj.employee_age))
+				}
+
+				setChartData({
+					labels: [day5, day4, day3, day2, day1],
+					datasets: [{
+						label: 'Average Points Earned Daily',
+						data: snackPoints,
+						backgroundColor: function(context)
 						{
-							label: '# of Votes',
-							data: [12, 19, 3, 5, 2, 3],
-							backgroundColor: [
-								'rgba(255, 99, 132, 0.2)',
-								'rgba(54, 162, 235, 0.2)',
-								'rgba(255, 206, 86, 0.2)',
-								'rgba(75, 192, 192, 0.2)',
-								'rgba(153, 102, 255, 0.2)',
-								'rgba(255, 159, 64, 0.2)',
-							],
-							borderColor: [
-								'rgba(255, 99, 132, 1)',
-								'rgba(54, 162, 235, 1)',
-								'rgba(255, 206, 86, 1)',
-								'rgba(75, 192, 192, 1)',
-								'rgba(153, 102, 255, 1)',
-								'rgba(255, 159, 64, 1)',
-							],
-							borderWidth: 1,
+							var index = context.dataIndex;
+							var value = context.dataset.data[index];
+
+							return value >= 9 ? 'rgba(144,238,144, 0.5)' :
+								value < 5 ?  'rgba(205, 92, 92, 0.5)'   :
+									'rgba(255,228,181, 0.5)';
+
 						},
-					],
-				}}
-				width={600}
-				height={400}
-			/>
+						borderColor: function(context)
+						{
+							var index = context.dataIndex;
+							var value = context.dataset.data[index];
+
+							return value >= 9 ? 'rgba(144,238,144, 1.0)' :
+								value < 5 ?  'rgba(205, 92, 92, 1.0)'   :
+									'rgba(255,228,181, 1.0)';
+
+						},
+						borderWidth: 1,
+						borderRadius: [5],
+					}
+					]
+				})
+
+			})
+			.catch(err => {
+				console.log(err)
+			})
+
+		console.log(snackPoints)
+
+
+	}
+
+	useEffect(() =>
+	{
+		chart()
+	},[]);
+
+	return (
+		<div className = "grid">
+			<div className= 'container' >
+				<h3>
+					Snack Details
+					<form>
+						<label>
+							Today :
+							<input type="text" name="score" />
+						</label>
+						<input type="submit" value="Submit" />
+					</form>
+				</h3>
+			</div>
+
+			<div className='container'>
+
+				<div style ={{height: "300px", width: "700px"}}>
+					<Bar data={chartData} options = {{
+						responsive: true,
+						title : {text: 'Points Earned', display: true},
+						scales: {
+							yAxis: {
+								max: 10,
+								min: 0
+							}
+						}
+					}} />
+				</div>
+			</div>
 		</div>
-	);
+	)
+
 };
 
 export default SnackGraph;
