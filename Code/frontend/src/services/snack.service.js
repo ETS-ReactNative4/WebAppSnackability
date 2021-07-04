637de4c8f33ad204e2ce8301369de8f111f9ba9f
4UR3L10 <aureliomartinezdros@outlook.com>
2021-07-04 18:22:36 -0400

    Update the Snack Service File

--- Code/frontend/src/services/snack.service.js
+++ Code/frontend/src/services/snack.service.js
@@ -1,23 +1,5 @@
 import axios from 'axios';
 
-/*
-export function fetchSnacksList() {
-    return axios.get('http://localhost:4000/snacks');
-}
-
-export function fetchSnacksByName(keyword) {
-    return axios.get('http://localhost:4000/snacks', {
-        params: {
-            q: keyword
-        }
-    });
-}
-
-export function fetchSnackByID(snack_id) {
-    return axios.get(`http://localhost:4000/snacks/${snack_id}`);
-}
-*/
-
 export function fetchSnacksListUSDA() {
     return axios.get('http://localhost:4000/usda/snacks');
 }
