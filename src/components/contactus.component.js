203e9e42414a4dd0ef89130149c6296adf13230a
JohnyP1406 <56031609+JohnyP1406@users.noreply.github.com>
2021-09-03 21:48:05 -0400

    feat(ContactUs) - Added first and last name input groups to the Contact Us page

--- src/components/contactus.component.js
+++ src/components/contactus.component.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import {
+    Alert,
+    Button,
+    Card, Col, Container, Form,
+    FormControl,
+    FormLabel,
+    InputGroup,
+    Row,
+} from 'react-bootstrap';
 
 class ContactUs extends Component {
     render() { 
         return (
-        <div>
-            <p>Hello World!</p>
-        </div>
+            <Container className="mt-3">
+            <Row className="justify-content-md-center">
+                <Col xs={12} sm={7}>
+                    <Card>
+                        <Card.Body>
+                            <Form>
+                                <Row>
+                                    <Col>
+                                        <Form.Label>First Name</Form.Label>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control placeholder="John" />
+                                        </InputGroup>
+                                    </Col>
+                                    <Col>
+                                        <Form.Label>Last Name</Form.Label>
+                                        <InputGroup className="mb-3">
+                                            <Form.Control placeholder="Smith" />
+                                        </InputGroup>
+                                    </Col>
+                                </Row>
+                                 
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
         );
     }
 }
