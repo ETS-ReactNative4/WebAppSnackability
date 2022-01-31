15db3f01926a249439ad1faab6a10973dfaac374
ricardocolom64 <ricardocolom64@gmail.com>
2022-01-31 16:55:49 -0500

    feature(achievements): added Achievement element to allow mapping of achievements later

--- src/components/snack-achievements.component.js
+++ src/components/snack-achievements.component.js
@@ -4,18 +4,21 @@ import { defaults } from "react-chartjs-2";
 import "../styles/achievements.css";
 import achievement_red_warrior from "../images/achievements/achievement-red-warrior.png";
 
-defaults.plugins.legend.position = "top";
-defaults.plugins.legend.title = {
-  display: true,
-  text: "Average Points Earned Daily",
-  font: {
-    size: 22,
-  },
-};
+const Achievement = ({ name, image, level, date }) => (
+  <tr className="achievement-row">
+    <th
+      scope="row"
+      className="achievement-title-row d-flex flex-row align-items-center text-center"
+    >
+      <img src={image} />
+      <div className="achievement-name m-3">{name}</div>
+    </th>
+    <td>{level}</td>
+    <td>{date}</td>
+  </tr>
+);
 
-defaults.plugins.legend.labels.boxWidth = 0;
-
-function SnackAchievements() {
+function SnackAchievements() {  
   return (
     <Container className="mt-3" id="achievements">
       <Row className="justify-content-md-center m-0">
@@ -32,28 +35,24 @@ function SnackAchievements() {
               </tr>
             </thead>
             <tbody>
-              <tr className="achievement-row">
-                <th
-                  scope="row"
-                  className="achievement-title-row d-flex flex-row align-items-center text-center"
-                >
-                  <img src={achievement_red_warrior} />
-                  <div className="achievement-name m-3">Red Warrior</div>
-                </th>
-                <td>12</td>
-                <td>01-28-2022</td>
-              </tr>
-              <tr className="achievement-row">
-                <th
-                  scope="row"
-                  className="achievement-title-row d-flex flex-row align-items-center text-center"
-                >
-                  <img src={achievement_red_warrior} />
-                  <div className="achievement-name m-3">Elite Warrior</div>
-                </th>
-                <td>12</td>
-                <td>01-28-2022</td>
-              </tr>
+            <Achievement
+                name="Red Warrior"
+                image={achievement_red_warrior}
+                level="99"
+                date="01-31-2022"
+              />
+              <Achievement
+                name="Golden Warrior"
+                image={achievement_red_warrior}
+                level="26"
+                date="01-25-2022"
+              />
+              <Achievement
+                name="Elite Warrior"
+                image={achievement_red_warrior}
+                level="12"
+                date="01-28-2022"
+              />
             </tbody>
           </Table>
         </Card>
